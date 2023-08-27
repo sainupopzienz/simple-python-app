61443261c660814ab9dc809ffb7873ce44438923
sainupopzienz <118021213+sainupopzienz@users.noreply.github.com>
2023-08-27 14:40:36 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sainupopzienz/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-doker run -d -p 5000:5000 sainupopzienz/simple-python-flask-app:latest
+docker run -d -p 5000:5000 sainupopzienz/simple-python-flask-app:latest
